8205781f38f4d16bc324c57a1b90dda8cc711469
TimCasserby <tim.casserby@elev.ga.ntig.se>
2022-03-13 18:16:36 +0100

    Förra lektionen
    Glömde publicera
    Har fixat javascript så nu animeras knappen för nav-en. Själva menyn måste fortfarande göras. Har börjat på Layouten för styrelse-sidans innehåll.

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+const burger = document.querySelector(".burger")
+// const nav = document.querySelector(".nav-links")
+
+function toggleMenu(){
+    // nav.classList.toggle("nav-active")
+    burger.classList.toggle("toggle")
+}
+
+burger.addEventListener("click", toggleMenu)
